6caa0f3039ef39fc98526eeea99d34cfcc5aa724
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-03 16:31:06 +0200

    UPD(5-excercise--solution)

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -32,6 +32,7 @@ const firstTeacher = teachers[0];
   console.log(firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa")
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
