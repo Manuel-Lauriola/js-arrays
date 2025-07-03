9a7b7ac915c3e1b319f722a12b06191ec5449c06
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-03 16:34:12 +0200

    UPD(7-excercise--solution)

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -39,8 +39,8 @@ teachers.unshift("Sarah")
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
-  console.log(teachers)
+const lewisIndex = teachers.indexOf("Lewis");
+  console.log(lewisIndex)
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
